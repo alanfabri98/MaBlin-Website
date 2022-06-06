192abae8bf6b61034af2790cff5e4cc14c7d7777
alan oña <afona2@espe.edu.ec>
2022-06-06 12:10:53 -0500

    Cuarto deploy

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/MaBlinWebsite'));
@@ -6,4 +6,10 @@ app.get('/*', (req,res) => {
     res.sendFile(__dirname+'/dist/MaBlinWebsite/index.html');
 });
 
+app.listen(process.env.PORT || 8080);*/
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/MablinWebsite'));
+app.get('/*', function(req,res) {res.sendFile( path.join(__dirname + '/dist/MablinWebsite/index.html') );});
 app.listen(process.env.PORT || 8080);
